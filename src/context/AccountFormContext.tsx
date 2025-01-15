a5524ec8a6f44013b7ef22ebac10647bdef02b6e
gabrielsantiag0 <gabrielleandrosantiago9@gmail.com>
2025-01-15 20:48:08 -0300

    study form using react native and typescript, with useState, react-navigation and much more.... still in the process of being finalized.

--- src/context/AccountFormContext.tsx
+++ src/context/AccountFormContext.tsx
@@ -1,9 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 export type AccountProps ={
     name?: string;
+    email?: string;
+    phone?: string;
+    birth?: string;
+    password?: string;
+    passwordConfirm?: string;
 }
 
 type AccountFormContextDateProps ={
-    accountFormData: 
+    accountFormData: AccountProps;
+}
+
+type AccountFormProviderProps ={
+    children: ReactNode;
+}
+
+const accountFormContext = createContext<AccountFormContextDateProps>({} as AccountFormContextDateProps);
+
+function AccountProvider({ children }){
+
 }
